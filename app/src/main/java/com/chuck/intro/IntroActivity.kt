95396db8ab74fead629ff2f5b520e28449edcace
kk2013 <jo.x.santamaria@gmail.com>
2019-10-16 13:55:09 +0800

    Nav changes

--- app/src/main/java/com/chuck/intro/IntroActivity.kt
+++ app/src/main/java/com/chuck/intro/IntroActivity.kt
@@ -1,17 +1,26 @@
 package com.chuck.intro
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.chuck.R
 import com.chuck.databinding.ActivityIntroBinding
+import kotlinx.android.synthetic.main.activity_intro.nav_view
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +31,23 @@ class IntroActivity : AppCompatActivity() {
         )
         drawerLayout = binding.drawerLayout
 
-        val navController = Navigation.findNavController(this, R.id.nav_fragment)
+        val navController = findNavController(this, R.id.nav_fragment)
 
         // Set up ActionBar
         setSupportActionBar(binding.toolbar)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration =
+            AppBarConfiguration.Builder(R.id.main_fragment)
+                .setDrawerLayout(drawerLayout)
+                .build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupWithNavController(binding.navView, navController)
+//        setupActionBarWithNavController(this, navController, drawerLayout)
     }
 
-/*    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(drawerLayout,
-            Navigation.findNavController(this, R.id.nav_fragment))
-    }*/
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(this, R.id.nav_fragment)
+        return navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
+    }
 
     override fun onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
