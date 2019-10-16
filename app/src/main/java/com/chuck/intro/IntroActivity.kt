00161750553c6d99ef368256351b150c6b315535
kk2013 <jo.x.santamaria@gmail.com>
2019-10-16 22:19:49 +0800

    Add splash screen

--- app/src/main/java/com/chuck/intro/IntroActivity.kt
+++ app/src/main/java/com/chuck/intro/IntroActivity.kt
@@ -1,7 +1,6 @@
 package com.chuck.intro
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
@@ -9,20 +8,17 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.navigateUp
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.chuck.R
 import com.chuck.databinding.ActivityIntroBinding
-import kotlinx.android.synthetic.main.activity_intro.nav_view
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
 
         val binding: ActivityIntroBinding = DataBindingUtil.setContentView(
@@ -41,7 +37,6 @@ class IntroActivity : AppCompatActivity() {
                 .build()
         setupActionBarWithNavController(navController, appBarConfiguration)
         setupWithNavController(binding.navView, navController)
-//        setupActionBarWithNavController(this, navController, drawerLayout)
     }
 
     override fun onSupportNavigateUp(): Boolean {
